1f17e1e7654457e1eed4ea1785995efeea6adedf
Abubakkar Ghaswala <abughaswala.dev@gmail.com>
2026-09-06 17:10:51 +0530

    Fix Render server network binding

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,6 +39,6 @@ app.get('/health', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT as number, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
